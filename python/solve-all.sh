10b7ad08d5961b27d5da988310eb7b5a6594ac9f
Lukas J. Wensby <wensby@gmail.com>
2019-03-21 13:24:18 +0100

    Ignore entire input directories instead of files
    Ignoring all files that just ends with .input caused the test input
    files for the c solutions to not get included

--- python/solve-all.sh
+++ python/solve-all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Solving Day 1 - Part 1"
-cat 2015_01.input | python3.7 2015_01_1.py
+cat input/2015_01.input | python3.7 2015_01_1.py
 echo "Solving Day 1 - Part 2"
-cat 2015_01.input | python3.7 2015_01_2.py
+cat input/2015_01.input | python3.7 2015_01_2.py
 echo "Solving Day 2 - Part 1"
-cat 2015_02.input | python3.7 2015_02_1.py
+cat input/2015_02.input | python3.7 2015_02_1.py
 echo "Solving Day 2 - Part 2"
-cat 2015_02.input | python3.7 2015_02_2.py
+cat input/2015_02.input | python3.7 2015_02_2.py
