19fce861d325e1ae53d40dce3d43f73c9caf5cfa
Lukas J. Wensby <wensby@gmail.com>
2019-12-27 13:28:33 +0100

    Adjust 2019 Day 16 Part 1 in Python

--- python/2019_16_1.py
+++ python/2019_16_1.py
@@ -9,20 +9,23 @@ def run(input):
 def fft(signal, phases=1):
   signal = list(map(int, signal))
   for i in range(phases):
-    signal = list(map(lambda x: apply_pattern(signal, x), range(len(signal))))
-  return signal
+    signal = fft_once(signal)
+  return ''.join(map(str, signal))
+
+def fft_once(signal):
+  return list(map(lambda x: apply_pattern(signal, x), range(len(signal))))
 
 def apply_pattern(signal, position):
   def pattern_generator(position):
-    i = 0
+    i = position
     while True:
       i += 1
       yield base_pattern[int(i // (1+position)) % 4]
   generator = pattern_generator(position)
   sum = 0
-  for signal_digit in signal:
+  for signal_index in range(position, len(signal)):
     next_pattern_value = next(generator)
-    sum += signal_digit * next_pattern_value
+    sum += signal[signal_index] * next_pattern_value
   return abs(sum) % 10
 
 if __name__ == '__main__':
