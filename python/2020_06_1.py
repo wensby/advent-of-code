4f2f19f76254b4ac95008661a9bde5b39ad1245e
Lukas J. Wensby <wensby@gmail.com>
2020-12-06 10:21:47 +0100

    Solve 2020 Day 6 Part 1 in Python

---
+++ python/2020_06_1.py
@@ -0,0 +1,23 @@
+import sys
+
+def run(input):
+  groups = parse_groups(input)
+  sum = 0
+  for group in groups:
+    sum += len(set().union(*group))
+  return sum
+
+def parse_groups(input):
+  groups = []
+  group = []
+  groups.append(group)
+  for line in input.splitlines():
+    if not line:
+      group = []
+      groups.append(group)
+    else:
+      group.append(set(line))
+  return groups
+
+if __name__ == '__main__':
+  print(run(sys.stdin.read()))
