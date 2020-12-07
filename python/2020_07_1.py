ce62316c134d85ef8edc15a49c6a5536e95f6db2
Lukas J. Wensby <wensby@gmail.com>
2020-12-07 22:03:12 +0100

    Solve 2020 Day 7 Part 1 in Python

---
+++ python/2020_07_1.py
@@ -0,0 +1,35 @@
+import sys
+
+def run(input):
+  rules = parse_rules(input)
+  final_contents = resolve_final_contents(rules)
+  return len([bag for bag, contents in final_contents.items() if 'shiny gold' in contents])
+
+def parse_rules(input):
+  return {parse_bagtype(line): parse_content(line) for line in input.splitlines()}
+
+def parse_bagtype(line):
+  return ' '.join(line.split(' ')[:2])
+
+def parse_content(line):
+  if 'no other bags' in line:
+    return dict()
+  content_part = line.split(' contain ')[1][:-1].replace(' bags', '').replace(' bag', '')
+  count_by_bag_colors = {' '.join(x.split(' ')[1:]): int(x.split(' ')[0]) for x in content_part.split(', ')}
+  return count_by_bag_colors
+
+def resolve_final_contents(rules):
+  return {bag: get_contents(rules, 1, bag) for bag in rules}
+    
+def get_contents(rules, count, bag):
+  direct_contents = rules[bag]
+  count_adjusted = {b: c * count for b, c in direct_contents.items()}
+  result = {k: v for k, v in count_adjusted.items()}
+  for contained_bag, count in count_adjusted.items():
+    contained_bags_content = get_contents(rules, count, contained_bag)
+    for k, v in contained_bags_content.items():
+      result[k] = result.get(k, 0) + v
+  return result
+
+if __name__ == '__main__':
+  print(run(sys.stdin.read()))
---
+++ python/test_2020_07_1.py
@@ -0,0 +1,20 @@
+import importlib
+import unittest
+
+solution = importlib.import_module('2020_07_1')
+
+class Test2020Day7Part1(unittest.TestCase):
+
+  def test_example1(self):
+    input = (
+        'light red bags contain 1 bright white bag, 2 muted yellow bags.\n'
+        'dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n'
+        'bright white bags contain 1 shiny gold bag.\n'
+        'muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n'
+        'shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n'
+        'dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n'
+        'vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n'
+        'faded blue bags contain no other bags.\n'
+        'dotted black bags contain no other bags.\n'
+    )
+    self.assertEqual(solution.run(input), 4)
